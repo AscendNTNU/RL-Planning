9a94fa6cc6d3f495e9fe9e66d4de592076ee413d
Unknown <rasmusmunter@gmail.com>
2017-10-17 21:25:30 +0200

    Clearer DQN single robot

--- DQN/DQN-Single-Robot.py
+++ DQN/DQN-Single-Robot.py
@@ -9,7 +9,7 @@ import subprocess
 from ctypes import *
 from time import time
 
-_sim = CDLL('./PythonAccessToSim.so')
+_sim = CDLL('../cppWrapper/PythonAccessToSim.so')
 
 Num_Targets =_sim.get_sim_Num_Targets()
 D = 3+4 # input dimensionality
@@ -44,34 +44,30 @@ class Qnetwork():
 		self.keep_per = tf.placeholder(shape=(),dtype=tf.float32)
 		self.hidden = slim.fully_connected(self.inputs,h_size,biases_initializer=None,weights_initializer = initializer, activation_fn=tf.nn.relu)
 		self.hidden = slim.dropout(self.hidden,self.keep_per)
-		self.keep_per2 = tf.placeholder(shape=(),dtype=tf.float32)
-		self.hidden2 = slim.fully_connected(self.hidden,int(h_size/2),biases_initializer=None,weights_initializer = initializer, activation_fn=tf.nn.relu)
-		self.hidden2 = slim.dropout(self.hidden2, self.keep_per2)
-		#self.hidden3 = slim.fully_connected(self.hidden2,h_size,biases_initializer=None,weights_initializer = initializer, activation_fn=tf.nn.relu)
-		self.streamAC,self.streamVC = tf.split(1,2,self.hidden2)
+
+		self.streamAC,self.streamVC = tf.split(1,2,self.hidden)
 		self.streamA = tf.contrib.layers.flatten(self.streamAC)
 		self.streamV = tf.contrib.layers.flatten(self.streamVC)
-		self.AW = tf.Variable(tf.random_normal([int(h_size/4), a_size]))
-		self.VW = tf.Variable(tf.random_normal([int(h_size/4), 1]))
+		self.AW = tf.Variable(tf.random_normal([int(h_size/2), a_size]))
+		self.VW = tf.Variable(tf.random_normal([int(h_size/2), 1]))
 		self.Advantage = tf.matmul(self.streamA,self.AW)
 		self.Value = tf.matmul(self.streamV,self.VW)
 		
-
-		self.Temp = tf.placeholder(shape=(), dtype = tf.float32)
 		#Then combine them together to get our final Q-values.
 		self.Qout = self.Value + tf.sub(self.Advantage,tf.reduce_mean(self.Advantage,reduction_indices=1,keep_dims=True))
-		self.Q_dist = slim.softmax(self.Qout/self.Temp)
+		self.Q_dist = slim.softmax(self.Qout)
 		self.predict = tf.argmax(self.Qout,1)
+
 		#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
 		self.targetQ = tf.placeholder(shape=[None],dtype=tf.float32)
 		self.actions = tf.placeholder(shape=[None],dtype=tf.int32)
 		self.actions_onehot = tf.one_hot(self.actions, a_size, dtype=tf.float32)
 		
-		self.Q = tf.reduce_sum(tf.mul(self.Qout, self.actions_onehot), reduction_indices=1)
+		self.Q = tf.reduce_sum(tf.mul(self.Qout, self.actions_onehot), axis=1)
 		
 		self.td_error = tf.square(self.targetQ - self.Q)
 		self.loss = tf.reduce_mean(self.td_error)
-		self.trainer = tf.train.AdamOptimizer(learning_rate=0.0001) ## replace with variable TODO
+		self.trainer = tf.train.AdamOptimizer(learning_rate=0.0001)
 		self.updateModel = self.trainer.minimize(self.loss)
 
 class experience_buffer():
@@ -93,12 +89,12 @@ def resetGradBuffer(gradBuffer):
 		gradBuffer[ix] = grad * 0
 	return gradBuffer
 		
-def discount_rewards(r):
+def discount_rewards(reward):
 	""" take 1D float array of rewards and compute discounted reward """
-	discounted_r = np.zeros_like(r)
+	discounted_r = np.zeros_like(reward)
 	running_add = 0
-	for t in reversed(xrange(0, r.size)):
-		running_add = running_add * gamma + r[t]
+	for t in reversed(xrange(0, reward.size)):
+		running_add = running_add * gamma + reward[t]
 		discounted_r[t] = running_add
 	return discounted_r
 
@@ -115,9 +111,7 @@ def updateTarget(op_holder,sess):
 		sess.run(op)
 
 def observation_to_input_array(ai_view):
-	#print(ai_view.elapsed_time)
 	result = np.array([ai_view.elapsed_time%20, ai_view.drone_x/20.0, ai_view.drone_y/20.0])
-	# for i in range(Num_Targets):
 	result = np.append(result, ai_view.target_x[0]/20.0)
 	result = np.append(result, ai_view.target_y[0]/20.0)
 	result = np.append(result, ai_view.target_q[0]/(3.14159265359*2))
@@ -125,7 +119,6 @@ def observation_to_input_array(ai_view):
 	return result
 
 def target_observation_to_input_array(ai_view, target):
-	#print(ai_view.elapsed_time)
 	result = np.array([ai_view.elapsed_time%20, ai_view.drone_x/20.0, ai_view.drone_y/20.0])
 	result = np.append(result, ai_view.target_x[target]/20.0)
 	result = np.append(result, ai_view.target_y[target]/20.0)
@@ -144,40 +137,35 @@ def chooseRobot(ai_view):
 
 tf.reset_default_graph() #Clear the Tensorflow graph.
 
-batch_size = 50 #How many experiences to use for each training step.
-update_freq = 4 #How often to perform a training step.
-y = .995 #Discount factor on the target Q-values
-startE =  0.1#Starting chance of random action
-endE = 0.1 #Final chance of random action
-anneling_steps = 1000000 #How many steps of training to reduce startE to endE.
-num_episodes = 10000000#How many episodes of game environment to train network with.
-pre_train_steps = 1000000 #How many steps of random actions before training begins.
-#max_epLength = 6*10 #The max allowed length of our episode.
-load_model = True#Whether to load a saved model.
-path = "./dqn1" #The path to save our model to.
-h_size = 256#Hidden layer size
-tau = 0.001 #Rate to update target network toward primary network
-learning_rate = 1e-4
+batch_size = 50 			#How many experiences to use for each training step.
+update_freq = 4 			#How often to perform a training step.
+y = .995 					#Discount factor on the target Q-values
+startE =  0.1				#Starting chance of random action
+endE = 0.1 					#Final chance of random action
+anneling_steps = 1000000 	#How many steps of training to reduce startE to endE.
+num_episodes = 10000000		#How many episodes of game environment to train network with.
+pre_train_steps = 1000000 	#How many steps of random actions before training begins.
+path = "./dqn1" 			#The path to save our model to.
+h_size = 256				#Hidden layer size
+tau = 0.001					#Rate to update target network toward primary network
+
+learning_rate = 1e-4		
 action_pool = list(range(0,3))
 
-training = False
+load_model = True			#Whether to load a saved model.
+training = False			#Whether to train or not.
 
-render = False
+
 tf.reset_default_graph()
 
-#cell = tf.nn.rnn_cell.LSTMCell(num_units=h_size,state_is_tuple=True)
 mainQN = Qnetwork(lr=learning_rate,s_size=D,a_size=len(action_pool),h_size=h_size, batch_size = batch_size, myScope = "main")
 targetQN = Qnetwork(lr=learning_rate,s_size=D,a_size=len(action_pool),h_size=h_size, batch_size = batch_size,  myScope = "target") #Load the agent.
 
 
 init = tf.global_variables_initializer()
-
 saver = tf.train.Saver()
-
 trainables = tf.trainable_variables()
-
 targetOps = updateTargetGraph(trainables,tau)
-
 myBuffer = experience_buffer()
 
 #Set the rate of random action decrease. 
@@ -201,119 +189,97 @@ with tf.Session() as sess:
 			saver.restore(sess,ckpt.model_checkpoint_path)
 		else:
 			sess.run(init)
+
 		summary_writer = tf.summary.FileWriter("dqn1_summary")
-		
 		updateTarget(targetOps,sess) #Set the target network to be equal to the primary network.
-		if render:
-			_sim.initialize_gui()
 
 		for i in range(num_episodes):
 			episodeBuffer = experience_buffer()
+
 			#Reset environment and get first new observation
 			last_time = 0
-			if render:
-				#TODO:COULD CAUSE PROBLEMS
-				process = subprocess.Popen("build/sim")
-				step = _sim.recieve_state_gui()
-				while step.ai_data_input.elapsed_time - last_time < 2:
-					step = _sim.recieve_state_gui()
-				last_time = step.ai_data_input.elapsed_time
-			else:
+			_sim.initialize()
+			step = _sim.step()
+
+			while(step.ai_data_input.drone_x < 0):
 				_sim.initialize()
 				step = _sim.step()
 
-				while(step.ai_data_input.drone_x < 0):
-					sim.initialize()
-					step = _sim.step()
-
-			s = observation_to_input_array(step.ai_data_input)
-			d = False
+			state = observation_to_input_array(step.ai_data_input)
+			done = False
 			xList = []
 			rAll = 0
 			j = 0
 			#The Q-Network
 			while True: #If the agent takes longer than max time, end the trial.
 				j+=1
-				#print(s[0])
-				a = sess.run(mainQN.predict,feed_dict={mainQN.inputs:[s],mainQN.keep_per2:(1-e)+0.1, mainQN.keep_per:(1-e)+0.1})
-				a = a[0]
-				r = 0 #_sim.action_rewards(action_pool[a])/1000;
+				#print(state[0])
+				action = sess.run(mainQN.predict,feed_dict={mainQN.inputs:[state],mainQN.keep_per2:(1-e)+0.1, mainQN.keep_per:(1-e)+0.1})
+				action = action[0]
+				reward = 0 #_sim.action_rewards(action_pool[action])/1000;
 
-				if render:
-					_sim.send_command_gui(action_pool[a])
-					step = _sim.recieve_state_gui()
-					while step.ai_data_input.elapsed_time - last_time < 5 or not step.cmd_done:
-						r += step.reward
-						step = _sim.recieve_state_gui()
-					last_time = step.ai_data_input.elapsed_time
-				else:
-					_sim.send_command(action_pool[a])
+				_sim.send_command(action_pool[action])
+				step = _sim.step()
+				while not step.cmd_done:
+					reward += step.reward
 					step = _sim.step()
-					while not step.cmd_done:
-						r += step.reward
-						step = _sim.step()
 					
+				next_state = observation_to_input_array(step.ai_data_input);
 
-				s1 = observation_to_input_array(step.ai_data_input);
-
-				r += step.reward
-				d = step.done
+				reward += step.reward
+				done = step.done
 				total_steps += 1
-				# episodeBuffer.add(np.reshape(np.array([s,a,r,s1,d]),[1,5])) #Save the experience to our episode buffer.
+				episodeBuffer.add(np.reshape(np.array([state,action,reward,next_state,done]),[1,5])) #Save the experience to our episode buffer.
 
-				# if total_steps > pre_train_steps and not render:
-				# 	if e > endE:
-				# 		e -= stepDrop
-					
-				# 	if total_steps % (update_freq) == 0:
-				# 		trainBatch = myBuffer.sample(batch_size) #Get a random batch of experiences.
-				# 		#Below we perform the Double-DQN update to the target Q-values
-				# 		Q1 = sess.run(mainQN.predict,feed_dict={mainQN.inputs:np.vstack(trainBatch[:,3]),mainQN.keep_per2:1.0, mainQN.keep_per:1.0})
-				# 		Q2 = sess.run(targetQN.Qout,feed_dict={targetQN.inputs:np.vstack(trainBatch[:,3]),targetQN.keep_per2:1.0, targetQN.keep_per:1.0})
-				# 		end_multiplier = -(trainBatch[:,4] - 1)
-				# 		doubleQ = Q2[range(batch_size),Q1]
-				# 		targetQ = trainBatch[:,2] + (y*doubleQ * end_multiplier)
-				# 		#Update the network with our target values.
-				# 		_ = sess.run(mainQN.updateModel,
-				# 			feed_dict={mainQN.inputs:np.vstack(trainBatch[:,0]),mainQN.targetQ:targetQ, mainQN.actions:trainBatch[:,1],mainQN.keep_per2:1.0, mainQN.keep_per:1.0})
+				# If we are done with experience collection start updating Q network
+				if total_steps > pre_train_steps:
+					if e > endE:
+						e -= stepDrop
+
+					if total_steps % update_freq == 0:
+						trainBatch = myBuffer.sample(batch_size) #Get action random batch of experiences.
 						
-				# 		updateTarget(targetOps,sess) #Set the target network to be equal to the primary network.
-				rAll += r
-				s = s1
+						#Below we perform the Double-DQN update to the target Q-values
+						Q1 = sess.run(mainQN.predict,feed_dict={mainQN.inputs:np.vstack(trainBatch[:,3]),mainQN.keep_per2:1.0, mainQN.keep_per:1.0})
+						Q2 = sess.run(targetQN.Qout,feed_dict={targetQN.inputs:np.vstack(trainBatch[:,3]),targetQN.keep_per2:1.0, targetQN.keep_per:1.0})
+						end_multiplier = -(trainBatch[:,4] - 1)
+						doubleQ = Q2[range(batch_size),Q1]
+						targetQ = trainBatch[:,2] + (y*doubleQ * end_multiplier)
+						
+						#Update the network with our target values.
+						_ = sess.run(mainQN.updateModel,
+							feed_dict={mainQN.inputs:np.vstack(trainBatch[:,0]),mainQN.targetQ:targetQ, mainQN.actions:trainBatch[:,1],mainQN.keep_per2:1.0, mainQN.keep_per:1.0})
+						
+						updateTarget(targetOps,sess) #Set the target network to be equal to the primary network.
+				
+				rAll += reward
+				state = next_state
 
-				if d == True:
-					# print("here")
+				if done == True:
 					print(step.ai_data_input.elapsed_time)
 					break
-
-			
-			if render:
-				print("killing")
-				process.kill()
-				print(rAll)
 
 			#Get all experiences from this episode and discount their rewards.
 			myBuffer.add(episodeBuffer.buffer)
 			jList.append(j)
 			rList.append(rAll)
-			#print(rAll)
 			#Periodically save the model. 
 			if i % 50 == 0 and i != 0 and not render:
 
 				mean_reward = np.mean(rList[-50:])
 				mean_length = np.mean(jList[-50:])
-			# 	summary = tf.Summary()
-			# 	summary.value.add(tag='Perf/Reward', simple_value=float(mean_reward))
-			# 	summary.value.add(tag='Perf/Length', simple_value=float(mean_length))
+				summary = tf.Summary()
+				summary.value.add(tag='Perf/Reward', simple_value=float(mean_reward))
+				summary.value.add(tag='Perf/Length', simple_value=float(mean_length))
 
-			# 	summary_writer.add_summary(summary, i)
+				summary_writer.add_summary(summary, i)
 				print(i, "Mean reward: ", mean_reward, " Mean steps: ", mean_length, " e: ", e)
 
-			# 	summary_writer.flush( 	)
+				summary_writer.flush( 	)
 
-			# if i % 1000 == 0 and i != 0 and not render:
-			# 	saver.save(sess,path+'/model-'+str(i)+'.cptk')
-			# 	print("Saved Model")
+			if i % 1000 == 0 and i != 0 and not render:
+				saver.save(sess,path+'/model-'+str(i)+'.cptk')
+				print("Saved Model")
 		
 	else:
 		print('Loading Model...')
@@ -335,8 +301,8 @@ with tf.Session() as sess:
 
 
 			target = chooseRobot(step.ai_data_input)
-			s = target_observation_to_input_array(step.ai_data_input,target)
-			d = False
+			state = target_observation_to_input_array(step.ai_data_input,target)
+			done = False
 			xList = []
 			rAll = 0
 			j = 0
@@ -344,14 +310,14 @@ with tf.Session() as sess:
 			while True: #If the agent takes longer than max time, end the trial.
 				j+=1
 
-				a = sess.run(mainQN.predict,feed_dict={mainQN.inputs:[s],mainQN.keep_per2:1, mainQN.keep_per:1})
-				a = a[0]
-				r = 0 #_sim.action_rewards(action_pool[a])/1000;
+				action = sess.run(mainQN.predict,feed_dict={mainQN.inputs:[state],mainQN.keep_per2:1, mainQN.keep_per:1})
+				action = action[0]
+				reward = 0 #_sim.action_rewards(action_pool[action])/1000;
 
-				_sim.target_send_command_gui(action_pool[a], target)
+				_sim.target_send_command_gui(action_pool[action], target)
 				step = _sim.recieve_state_gui()
 				while step.ai_data_input.elapsed_time - last_time < 5 or not step.cmd_done:
-					r += step.reward
+					reward += step.reward
 					step = _sim.recieve_state_gui()
 					print("stepping")
 					print(step.ai_data_input.target_y[target])
@@ -362,15 +328,15 @@ with tf.Session() as sess:
 					print("choosing new robot")
 					print(step.ai_data_input.target_y[target])	
 					target = chooseRobot(step.ai_data_input)
-				s1 = target_observation_to_input_array(step.ai_data_input,target);
+				next_state = target_observation_to_input_array(step.ai_data_input,target);
 				
-				r += step.reward
-				d = step.done
+				reward += step.reward
+				done = step.done
 				total_steps += 1
-				rAll += r
-				s = s1
+				rAll += reward
+				state = next_state
 
-				if d == True:
+				if done == True:
 					print("done")
 					break
 			
